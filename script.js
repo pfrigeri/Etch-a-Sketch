2cca742f7c25616ebcac20e871270d7651589e86
pfrigeri <asockmf@gmail.com>
2025-10-03 15:54:12 -0300

    Generate div squares in a loop and add basic styling to the container and squares

---
+++ script.js
@@ -0,0 +1,9 @@
+const gridContainer = document.querySelector("#grid-container");
+
+for(let i = 0; i <= (16*16); i++){
+
+    const square = document.createElement("div");
+    square.classList.add('grid-square');
+
+    gridContainer.appendChild(square)
+}

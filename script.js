82f4075ebf555d59caf5f0773161e1b2c97cce8f
pfrigeri <asockmf@gmail.com>
2025-10-03 16:39:39 -0300

    Create a grid effect using gap and adding 1px to square size

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const gridContainer = document.querySelector("#grid-container");
 let gridW = gridContainer.clientWidth;
 let gridH = gridContainer.clientHeight;
 const squareSize = 16;
-let sqrPerColumn = Math.floor(gridH/squareSize);
-let sqrPerRow = Math.floor(gridW/squareSize);
+//Add +1 into square size for the gap property
+let sqrPerColumn = Math.floor(gridH/(squareSize + 1));
+let sqrPerRow = Math.floor(gridW/(squareSize + 1));
 
 for(let i = 0; i < (sqrPerColumn*sqrPerColumn); i++){
     const square = document.createElement("div");

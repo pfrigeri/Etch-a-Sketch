0c5b5f1f67410f607fb03c66d8e0b44420c6e6f0
pfrigeri <asockmf@gmail.com>
2025-10-03 16:32:24 -0300

    Make the squares fit perfectly into the container

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const gridContainer = document.querySelector("#grid-container");
+let gridW = gridContainer.clientWidth;
+let gridH = gridContainer.clientHeight;
+const squareSize = 16;
+let sqrPerColumn = Math.floor(gridH/squareSize);
+let sqrPerRow = Math.floor(gridW/squareSize);
 
-for(let i = 0; i <= (16*16); i++){
-
+for(let i = 0; i < (sqrPerColumn*sqrPerColumn); i++){
     const square = document.createElement("div");
     square.classList.add('grid-square');
 
